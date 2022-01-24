1bd95244d0ab552afef07f8032c847d38e713cbc
R-Andrei <faragau.rares@gmail.com>
2022-01-24 02:30:55 +0200

    Added entry point for serber + type declarations.

---
+++ src/types/User.ts
@@ -0,0 +1,19 @@
+import { Rooms } from "./Room";
+
+export interface User {
+    _id: string;
+    username: string;
+    tag: string;
+    email: string;
+    rooms: Rooms;
+    createdAt: Date;
+    updatedAt: Date;
+    status: string;
+}
+
+export interface SocketUser extends User {
+    socketId: string;
+}
+
+export type Users = User[];
+export type SocketUsers = SocketUser[];
